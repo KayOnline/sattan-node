2d3028b1c289d80efe2b24aab2a0ff4a7f870d10
Kay <21156929@qq.com>
2015-03-27 12:52:59 +0800

    rewrite content

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+/*
 var express = require('express');
 
 var app = new express();
@@ -8,3 +9,18 @@ app.get('/test', function(req, res) {
 app.listen(process.env.PORT || 5000, function() {
   console.log('server is running');
 })
+*/
+
+var express = require('express');
+var app = express();
+
+app.set('port', (process.env.PORT || 5000));
+app.use(express.static(__dirname + '/public'));
+
+app.get('/', function(request, response) {
+  response.send('Hello World!');
+});
+
+app.listen(app.get('port'), function() {
+  console.log("Node app is running at localhost:" + app.get('port'));
+});
