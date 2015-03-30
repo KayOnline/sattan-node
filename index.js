548c899e7234061e18e0cca0a2dc1c0c3ba6b247
Kay <21156929@qq.com>
2015-03-30 00:34:23 -0700

    use mongodb

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
 var express = require('express');
+var mongoose = require('mongoose');
+
+
+mongoose.connect('mongodb://192.168.214.137:27017/mydb');
+var User = mongoose.model('User', {name: String, age: Number});
+
+var user1 = new User({name: 'vita', age: 25});
+user1.save(function(err) {
+  if (err) console.info(err);
+});
 
 var app = new express();
 app.get('/test', function(req, res) {
