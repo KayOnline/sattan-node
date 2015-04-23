b62cb36d6feaac8eb2c6fbfc98daf4232ff64fc3
Kay <21156929@qq.com>
2015-04-23 11:39:38 +0800

    modify templates

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var about = require('./routes/about');
+var blog = require('./routes/blog');
+var support = require('./routes/support');
+var contact = require('./routes/contact');
 
 var app = express();
 
@@ -26,6 +29,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 app.use('/index', index);
 app.use('/about', about);
+app.use('/support', support);
+app.use('/contact', contact);
+app.use('/blog', blog);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
