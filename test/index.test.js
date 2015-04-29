4d32a76ced6e75491b639de6d09a8d4842b904fa
Kay <kay21156929@gmail.com>
2015-04-29 18:13:28 +0800

    Update index.test.js

--- test/index.test.js
+++ test/index.test.js
@@ -2,7 +2,7 @@ var app = require('../index.js');
 var supertest = require('supertest');
 
 var request = supertest(app);
-
+// add comment
 describe('test/index.test.js', function() {
   it('should return 64 when n == 8', function(done) {
     request.get('/square').query({n: 8}).expect('64', done);
