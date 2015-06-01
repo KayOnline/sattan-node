654011c25f1c1068dcb0d811a99651378232872e
Kay <21156929@qq.com>
2015-06-01 19:52:04 +0800

    bug fixed: no salt generate

--- routes/index.js
+++ routes/index.js
@@ -2,16 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 router.get('/', function(req, res, next) {
-	var articles = [{
-		title: "",
-		author: "",
-		content: "",
-	}, {
-		title: "",
-		author: "",
-		content: "",
-	}];
-	res.render('index', { title: 'index' });
+  res.render('index', { title: 'index' });
 });
 
 module.exports = router;
